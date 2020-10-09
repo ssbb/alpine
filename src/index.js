1171f646ac38dd26c0e101bb34af6ce9736f450f
Sviatoslav Bulbakha <ssbb@imac.local>
2020-10-09 13:11:57 +0300

    Fix component clone

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import Component from './component'
-import { domReady, isTesting } from './utils'
+import Component from "./component";
+import { domReady, isTesting } from "./utils";
 
 const Alpine = {
     version: process.env.PKG_VERSION,
@@ -15,121 +15,123 @@ const Alpine = {
     ignoreFocusedForValueBinding: false,
 
     start: async function () {
-        if (! isTesting()) {
-            await domReady()
+        if (!isTesting()) {
+            await domReady();
         }
 
-        this.discoverComponents(el => {
-            this.initializeComponent(el)
-        })
+        this.discoverComponents((el) => {
+            this.initializeComponent(el);
+        });
 
         // It's easier and more performant to just support Turbolinks than listen
         // to MutationObserver mutations at the document level.
         document.addEventListener("turbolinks:load", () => {
-            this.discoverUninitializedComponents(el => {
-                this.initializeComponent(el)
-            })
-        })
+            this.discoverUninitializedComponents((el) => {
+                this.initializeComponent(el);
+            });
+        });
 
-        this.listenForNewUninitializedComponentsAtRunTime(el => {
-            this.initializeComponent(el)
-        })
+        this.listenForNewUninitializedComponentsAtRunTime((el) => {
+            this.initializeComponent(el);
+        });
     },
 
     discoverComponents: function (callback) {
-        const rootEls = document.querySelectorAll('[x-data]');
+        const rootEls = document.querySelectorAll("[x-data]");
 
-        rootEls.forEach(rootEl => {
-            callback(rootEl)
-        })
+        rootEls.forEach((rootEl) => {
+            callback(rootEl);
+        });
     },
 
     discoverUninitializedComponents: function (callback, el = null) {
-        const rootEls = (el || document).querySelectorAll('[x-data]');
+        const rootEls = (el || document).querySelectorAll("[x-data]");
 
         Array.from(rootEls)
-            .filter(el => el.__x === undefined)
-            .forEach(rootEl => {
-                callback(rootEl)
-            })
+            .filter((el) => el.__x === undefined)
+            .forEach((rootEl) => {
+                callback(rootEl);
+            });
     },
 
     listenForNewUninitializedComponentsAtRunTime: function (callback) {
-        const targetNode = document.querySelector('body');
+        const targetNode = document.querySelector("body");
 
         const observerOptions = {
             childList: true,
             attributes: true,
             subtree: true,
-        }
+        };
 
         const observer = new MutationObserver((mutations) => {
             if (this.pauseMutationObserver) return;
 
-            for (let i=0; i < mutations.length; i++){
+            for (let i = 0; i < mutations.length; i++) {
                 if (mutations[i].addedNodes.length > 0) {
-                    mutations[i].addedNodes.forEach(node => {
+                    mutations[i].addedNodes.forEach((node) => {
                         // Discard non-element nodes (like line-breaks)
-                        if (node.nodeType !== 1) return
+                        if (node.nodeType !== 1) return;
 
                         // Discard any changes happening within an existing component.
                         // They will take care of themselves.
-                        if (node.parentElement && node.parentElement.closest('[x-data]')) return
+                        if (
+                            node.parentElement &&
+                            node.parentElement.closest("[x-data]")
+                        )
+                            return;
 
                         this.discoverUninitializedComponents((el) => {
-                            this.initializeComponent(el)
-                        }, node.parentElement)
-                    })
+                            this.initializeComponent(el);
+                        }, node.parentElement);
+                    });
                 }
-              }
-        })
+            }
+        });
 
-        observer.observe(targetNode, observerOptions)
+        observer.observe(targetNode, observerOptions);
     },
 
     initializeComponent: function (el) {
-        if (! el.__x) {
+        if (!el.__x) {
             // Wrap in a try/catch so that we don't prevent other components
             // from initializing when one component contains an error.
             try {
-                el.__x = new Component(el)
+                el.__x = new Component(el);
             } catch (error) {
                 setTimeout(() => {
-                    throw error
-                }, 0)
+                    throw error;
+                }, 0);
             }
         }
     },
 
-    clone: function (component, newEl) {
-        if (! newEl.__x) {
-            newEl.__x = new Component(newEl, component)
-        }
+    clone: function clone(component, newEl) {
+        newEl.__x = new Component(newEl, component.getUnobservedData());
     },
 
     addMagicProperty: function (name, callback) {
-        this.magicProperties[name] = callback
+        this.magicProperties[name] = callback;
     },
 
     onComponentInitialized: function (callback) {
-        this.onComponentInitializeds.push(callback)
+        this.onComponentInitializeds.push(callback);
     },
 
     onBeforeComponentInitialized: function (callback) {
-        this.onBeforeComponentInitializeds.push(callback)
-    }
-}
+        this.onBeforeComponentInitializeds.push(callback);
+    },
+};
 
-if (! isTesting()) {
-    window.Alpine = Alpine
+if (!isTesting()) {
+    window.Alpine = Alpine;
 
     if (window.deferLoadingAlpine) {
         window.deferLoadingAlpine(function () {
-            window.Alpine.start()
-        })
-   } else {
-        window.Alpine.start()
-   }
+            window.Alpine.start();
+        });
+    } else {
+        window.Alpine.start();
+    }
 }
 
-export default Alpine
+export default Alpine;
